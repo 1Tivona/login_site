b64495b0ca8a6d061e5919b5ae4c4b18ca8ceff5
TSpruill <tivona.spruill@yahoo.com>
2017-08-21 16:54:33 -0400

    first commit

---
+++ users.js
@@ -0,0 +1,20 @@
+module.exports = {
+ users: [
+   {
+      username: "GrannySmith",
+      password: "sweetAppleAcres"
+   },
+   {
+      username: "dimeo",
+      password: "password"
+   },
+    {
+      username: "AppleJack",
+      password: "pony"
+    },
+    {
+      username: "colin",
+      password: "password"
+    }
+ ]
+}
